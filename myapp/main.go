a6223b6b11192a8dc99afc53b79ce00af6f109d3
ahmadun <ahmadun.jambi@gmail.com>
2022-12-07 16:07:44 +0700

    first commit

---
+++ myapp/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"myapp/configs"
+	"myapp/routes"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+	}))
+
+	configs.ConnectDB()
+	routes.UserRoute(e)
+
+	e.Logger.Fatal(e.Start(":5000"))
+}
